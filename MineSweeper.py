eb26b68d1f4702f32ebf810e3e13d5d885229060
Salman <s.r.bisliev@gmail.com>
2024-09-01 10:15:39 +0200

    New frames, new buttons with events
    Have established 3 main frames, where as center frame has buttons. Buttons that are clickable with left mouse and right mouse. Also have made grid-based location of the buttons. Should make the button placement more easy to handle.

--- MineSweeper.py
+++ MineSweeper.py
@@ -1,15 +1,17 @@
 from tkinter import *
-
+import settings
+import utils
+from cell import Cell
 #--------------Window-----------
 root =Tk()
 
 #Ovveride the setting of the window
 
 #Change background color by using bg. - MUST use aviable values from a list.
-root.configure(bg="navyblue")
+root.configure(bg="black")
 
 #Size the window
-root.geometry("1024x780")
+root.geometry(f"{settings.WIDTH}x{settings.HEIGHT}")
 
 #window Title
 root.title("Mine Sweeper-2024")
@@ -18,29 +20,47 @@ root.title("Mine Sweeper-2024")
 root.resizable(False, False)
 
 
+#Frame at the top
 top_frame1of3=Frame(
     root,
-    bg="yellow", #Change later to navyblue
-    width=1024,
-    height=90
-)
-top_frame2of3=Frame(
-    root,
-    bg="red", #Change later to navyblue
-    width=1024,
-    height=90
-)
-top_frame3of3=Frame(
-    root,
-    bg="green", #Change later to navyblue
-    width=1024,
-    height=90
+    bg="black", #Change later to navyblue
+    width=settings.WIDTH,
+    height=utils.height_prct(25)
 )
 
 top_frame1of3.place(x=0,y=0)
-top_frame2of3.place(x=0,y=90)
-top_frame3of3.place(x=0,y=180)
 
+#frame at the left
+left_frame=Frame(
+    root,
+    bg="black",
+    width=utils.width_prct(25),
+    height=utils.height_prct(75)
+)
+
+left_frame.place(x=0,y=utils.height_prct(25))
+
+center_frame= Frame(
+    root,
+    bg="black",
+    width=utils.width_prct(75),
+    height=utils.height_prct(75)
+)
+
+center_frame.place(
+    x=utils.width_prct(25),
+    y=utils.height_prct(25))
+
+
+
+
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell()
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+            column=x, row=y
+        )
 #Run the window
 root.mainloop()
 #--------------Window-----------
